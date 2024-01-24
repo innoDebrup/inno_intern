d00b8e6bad932b1ecfe20ac2ebad95790d0db179
innoDebrup <debrup.pramanik@innoraft.com>
2024-01-24 15:32:03 +0530

    Test Website v2 finalized with new pricing section

--- test/test1/t1.js
+++ test/test1/t1.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  //default-----------------------------------------
+  $(".smc,.smj").hide();
+
+  //cross-------------------------------------------
   $("#cross1").click(function(){
     $("#i1").hide(1000);
     $("#price").addClass("sub_class");
@@ -11,6 +15,17 @@ $(document).ready(function(){
     $("#i3").hide(1000);
     $("#price").addClass("sub_class");
   })
+
+  //reset-------------------------------------------
+  $("#reset").click(function(){
+    $("#i1").show(1000);
+    $("#i2").show(1000);
+    $("#i3").show(1000);
+    $("#price").removeClass("sub_class");
+    
+  })
+
+  //nav_linking--------------------------------------
   $("#learn_link").click(function(){
     $("html").animate({scrollTop: $("#learn_sec").offset().top - 79},700)
   })
@@ -19,5 +34,23 @@ $(document).ready(function(){
   })
   $("#price_link").click(function(){
     $("html").animate({scrollTop: $("#price_sec").offset().top - 79},700)
+  })
+
+  //See more_less------------------------------------
+  $("#more_c").click(function(){
+    $(".slc").slideUp(200);
+    $(".smc").slideDown(200);
+  })
+  $("#less_c").click(function(){
+    $(".slc").slideDown(200);
+    $(".smc").slideUp(200);
+  })
+  $("#more_j").click(function(){
+    $(".slj").slideUp(200);
+    $(".smj").slideDown(200);
+  })
+  $("#less_j").click(function(){
+    $(".slj").slideDown(200);
+    $(".smj").slideUp(200);
   })
 });
